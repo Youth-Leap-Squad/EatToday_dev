a1a3bd5c1464f3593982806b526e4799ca3e7413
pilltong22 <rlawlsgh6@naver.com>
2025-09-23 17:24:41 +0900

    first commit

---
+++ src/main/java/com/eat/today/EatTodayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eat.today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatTodayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatTodayBackendApplication.class, args);
+    }
+
+}
