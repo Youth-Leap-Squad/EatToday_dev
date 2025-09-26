fcab44812681da2c4685f799a058a9b0c7037fa9
woo-kyoung-nam <dnrud6911@gmail.com>
2025-09-26 16:47:44 +0900

    Feat: #9 이벤트 albti, worldcup 조회 및 매퍼스캔 추가

---
+++ src/main/java/com/eat/today/event/albti/query/dto/AlbtiOutputDTO.java
@@ -0,0 +1,11 @@
+package com.eat.today.event.albti.query.dto;
+
+import lombok.Data;
+
+@Data
+public class AlbtiOutputDTO {
+    private int alBTI_no;
+    private String alBTI_alcohol_explain;
+    private int board_no;
+
+}
